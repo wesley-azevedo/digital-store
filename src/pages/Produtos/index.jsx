7ba8c8a36670f188469c75685fead1f1259c069f
wesley-azevedo <azevedosou@gmail.com>
2023-05-13 15:16:23 -0300

    fetch

--- src/pages/Produtos/index.jsx
+++ src/pages/Produtos/index.jsx
@@ -2,9 +2,13 @@ import estilo from './styles.module.scss'
 import Filtrar from '../../components/MenuFiltrar'
 import Produtos from '../../components/ProdutoCard'
 import Ordenar from '../../components/MenuOrdenar'
+import { useEffect } from 'react'
 export default function ProdutoHome() {
+    useEffect(()=>{console.log(1)
+    fetch('https://loja-backend.onrender.com/').then(res=>res.json()).then(data => console.log(data))
+    }, [])
     return (
-        <div className={estilo.teste}>
+        <div className={estilo.teste} style={{border: '1px red solid'}}>
             <div className={estilo.container}>
                 <div className={estilo.Ordenar}>
                     <Ordenar />
